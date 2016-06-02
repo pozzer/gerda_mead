03c7d2aa85b27ebf5a57aaa67e510c62fb0b4cf9
Feliphe Pozzer <feliphepozzer@gmail.com>
2016-06-02 12:35:13 -0300

    fix

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,8 +2,4 @@ class Rating < ActiveRecord::Base
 	belongs_to :user
   belongs_to :rateable, :polymorphic => true
 
-  has_reputation :commend,
-      source: :user,
-      aggregated_by: :sum
-      
 end
